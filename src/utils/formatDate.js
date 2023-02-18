af3141d068d7bd7edcf103c7ad92a0666c8e2640
ADIKryvonos <108137330+ADIKryvonos@users.noreply.github.com>
2023-02-18 16:59:10 +0200

    task4

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,6 +1,10 @@
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, format } from 'date-fns';
 
 export function formatDate(date) {
   const formatedDate = formatDistanceToNow(new Date(date), { addSuffix: true });
   return formatedDate;
+}
+
+export function dateTask(date) {
+  return format(new Date(date), 'Pp');
 }
